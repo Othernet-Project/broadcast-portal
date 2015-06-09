d6262bfb85f25b2ecb445526311c146f4ff722d1
Branko Vukelic <branko@brankovukelic.com>
2015-06-09 14:06:13 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+from broadcast import __version__
+
+
+SCRIPTDIR = os.path.dirname(__file__) or '.'
+PY3 = sys.version_info >= (3, 0, 0)
+
+
+def read(fname):
+    """ Return content of specified file """
+    path = os.path.join(SCRIPTDIR, fname)
+    if PY3:
+        f = open(path, 'r', encoding='utf8')
+    else:
+        f = open(path, 'r')
+    content = f.read()
+    f.close()
+    return content
+
+
+setup(
+    name='broadcast-portal',
+    version=__version__,
+    author='Outernet Inc',
+    author_email='apps@outernet.is',
+    description='Portal for submitting content to Outernet',
+    license='GPLv3',
+    keywords='broadcast, outernet, content, zip, json',
+    url='https://github.com/Outernet-Project/broadcast-portal',
+    packages=find_packages(),
+    long_description=read('README.rst'),
+    install_requires=read('conf/requirements.txt').strip().split('\n'),
+    entry_points={
+        'console_scripts': [
+            'runbroadcast = broadcast.app:main',
+        ]
+    },
+)
