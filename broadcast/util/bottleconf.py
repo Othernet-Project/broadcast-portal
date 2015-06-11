3dc36f4a946f9c54973adfdafa54d13dfe5c068d
Andrean Franc <andrean.franc@gmail.com>
2015-06-11 15:25:32 +0200

    Add necessary template tags to default template context

--- broadcast/util/bottleconf.py
+++ broadcast/util/bottleconf.py
@@ -2,14 +2,19 @@ import os
 
 import bottle
 
+import bottle_utils.csrf
 import bottle_utils.html
 
 
 def pre_init(config):
+    app = config['bottle']
     bottle.debug(config['server.debug'])
     bottle.TEMPLATE_PATH.insert(0, os.path.join(
         config['root'], config['app.view_path']))
     bottle.BaseTemplate.defaults.update({
         'request': bottle.request,
         'h': bottle_utils.html,
+        'url': app.get_url,
+        'csrf_tag': bottle_utils.csrf.csrf_tag,
+        '_': lambda x: x,
     })
