69ecd243c25aed6693b1fb14639f9f4334cebc88
Andrean Franc <andrean.franc@gmail.com>
2015-06-11 16:31:54 +0200

    Add email to registration form

--- broadcast/forms/auth.py
+++ broadcast/forms/auth.py
@@ -8,6 +8,8 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
+import re
+
 from bottle_utils import form
 from bottle_utils.i18n import lazy_gettext as _
 
@@ -16,8 +18,8 @@ from ..util import auth
 
 class LoginForm(form.Form):
     # Translators, used as label for a login field
-    username = form.StringField(_("Username"),
-                                placeholder=_('username'),
+    username = form.StringField(_("Username or E-mail"),
+                                placeholder=_('username or e-mail'),
                                 validators=[form.Required()])
     # Translators, used as label for a password field
     password = form.PasswordField(_("Password"),
@@ -39,6 +41,10 @@ class RegistrationForm(form.Form):
                                 validators=[form.Required()],
                                 placeholder=_('username'))
     # Translators, used as label in create user form
+    email = form.StringField(_("E-mail"),
+                             validators=[form.Required()],
+                             placeholder=_('e-mail'))
+    # Translators, used as label in create user form
     password1 = form.PasswordField(_("Password"),
                                    validators=[form.Required()],
                                    placeholder=_('password'))
@@ -46,6 +52,12 @@ class RegistrationForm(form.Form):
     password2 = form.PasswordField(_("Confirm Password"),
                                    validators=[form.Required()],
                                    placeholder=_('confirm password'))
+
+    def postprocess_email(self, value):
+        if not re.match(r'[^@]+@[^@]+\.[^@]+', value):
+            raise form.ValidationError(_("Invalid e-mail address entered."))
+
+        return value
 
     def validate(self):
         password1 = self.processed_data['password1']
