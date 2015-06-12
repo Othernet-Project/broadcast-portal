107ad2f69fc632b23c17e49fce7e60949dfea976
Andrean Franc <andrean.franc@gmail.com>
2015-06-12 15:59:54 +0200

    Allow email utilities to be used outside request context

--- broadcast/util/email.py
+++ broadcast/util/email.py
@@ -23,15 +23,16 @@ from bottle import template, request
 
 def send_multiple(to_list, subject, text=None, html=None, data={},
                   mandrill_args={'preserve_recipients': False},
-                  use_template=None):
+                  use_template=None, config=None):
     """ Sends out text/HTML email with specified templates """
-    conf = request.app.config
+    conf = config or request.app.config
 
     # Set up mandrill API access
     mandrill_client = mandrill.Mandrill(conf['mandrill.key'])
 
     # Prepare data for the email
-    parsed = urlparse(request.url)
+    url = request.url if config is None else conf.get('app.url', '')
+    parsed = urlparse(url)
     data['protocol'] = parsed.scheme
     data['host'] = parsed.netloc
     data['host_url'] = parsed.scheme + '://' + parsed.netloc
@@ -56,20 +57,20 @@ def send_multiple(to_list, subject, text=None, html=None, data={},
         try:
             return mandrill_client.messages.send_template(
                 template=use_template, message=message, async=False)
-        except Exception, e:
+        except Exception as e:
             logging.exception('Error sending email: %s' % e)
             return None
     else:
         try:
             return mandrill_client.messages.send(message=message, async=False)
-        except Exception, e:
+        except Exception as e:
             logging.exception('Error sending email: %s' % e)
             return None
 
 
 def send_mail(to, subject, text=None, html=None, to_name='',
               data={}, mandrill_args={'preserve_recipients': False},
-              use_template=None):
+              use_template=None, config=None):
     """ Send out text/HTML email with specified templates """
     return send_multiple([(to, to_name)], subject, text, html, data,
-                         mandrill_args, use_template)
+                         mandrill_args, use_template, config)
