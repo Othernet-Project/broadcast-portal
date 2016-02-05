3e0d622420f01ccf5319720bb284e8972328248f
Andrean Franc <andrean.franc@gmail.com>
2016-02-05 16:58:52 +0100

    Add touch-like feature and allow specification of parend id

--- broadcast/util/gdrive.py
+++ broadcast/util/gdrive.py
@@ -20,13 +20,13 @@ class DriveClient(object):
             return func(self, *args, **kwargs)
         return wrapper
 
-    def __init__(self, auth_data_path, owner_email):
+    def __init__(self, auth_data_path, owner_email=None):
         with open(auth_data_path) as auth_data_file:
             self._auth_data = json.load(auth_data_file)
 
         self._permission = {'type': 'user',
                             'role': 'writer',
-                            'value': owner_email}
+                            'value': owner_email} if owner_email else None
         self._service = None
 
     def _authenticate(self):
@@ -42,15 +42,31 @@ class DriveClient(object):
     def start(self):
         self._service = self._build_service()
 
-    @protect
-    def upload(self, file_path):
-        media_body = apiclient.http.MediaFileUpload(file_path, resumable=True)
-        body = {'title': os.path.basename(file_path)}
+    def _set_permission(self, file_id):
+        (self._service.permissions()
+             .insert(fileId=file_id, body=self._permission)
+             .execute())
+
+    def _upload(self, body, media_body=None, parent_id=None):
+        if parent_id:
+            body['parents'] = [{'id': parent_id}]
+
         file_data = (self._service.files()
                                   .insert(body=body, media_body=media_body)
                                   .execute())
-        (self._service.permissions()
-                      .insert(fileId=file_data['id'], body=self._permission)
-                      .execute())
+        if self._permission:
+            self._set_permission(file_data['id'])
+
         return file_data
+
+    @protect
+    def upload(self, file_path, parent_id=None):
+        media_body = apiclient.http.MediaFileUpload(file_path, resumable=True)
+        body = {'title': os.path.basename(file_path)}
+        return self._upload(body, media_body=media_body, parent_id=parent_id)
+
+    @protect
+    def touch(self, title, mime_type, parent_id=None):
+        body = {'title': title, 'mimeType': mime_type}
+        return self._upload(body, parent_id=parent_id)
 
