939bfb88be423394d0905eb9222e5c69e37f503e
Andrean Franc <andrean.franc@gmail.com>
2016-02-05 17:47:57 +0100

    Build service in constructor since they are used as one-off objects

--- broadcast/util/gdrive.py
+++ broadcast/util/gdrive.py
@@ -1,4 +1,3 @@
-import functools
 import json
 import os
 
@@ -12,14 +11,6 @@ from oauth2client.client import SignedJwtAssertionCredentials
 class DriveClient(object):
     SCOPE = 'https://www.googleapis.com/auth/drive'
 
-    def protect(func):
-        @functools.wraps(func)
-        def wrapper(self, *args, **kwargs):
-            if self._service is None:
-                raise RuntimeError("Call `start` before invoking this method.")
-            return func(self, *args, **kwargs)
-        return wrapper
-
     def __init__(self, auth_data_path, owner_email=None):
         with open(auth_data_path) as auth_data_file:
             self._auth_data = json.load(auth_data_file)
@@ -27,7 +18,7 @@ class DriveClient(object):
         self._permission = {'type': 'user',
                             'role': 'writer',
                             'value': owner_email} if owner_email else None
-        self._service = None
+        self._service = self._build_service()
 
     def _authenticate(self):
         creds = SignedJwtAssertionCredentials(self._auth_data['client_email'],
@@ -38,9 +29,6 @@ class DriveClient(object):
     def _build_service(self):
         http_auth = self._authenticate()
         return apiclient.discovery.build('drive', 'v2', http=http_auth)
-
-    def start(self):
-        self._service = self._build_service()
 
     def _set_permission(self, file_id):
         (self._service.permissions()
@@ -59,13 +47,11 @@ class DriveClient(object):
 
         return file_data
 
-    @protect
     def upload(self, file_path, parent_id=None):
         media_body = apiclient.http.MediaFileUpload(file_path, resumable=True)
         body = {'title': os.path.basename(file_path)}
         return self._upload(body, media_body=media_body, parent_id=parent_id)
 
-    @protect
     def touch(self, title, mime_type, parent_id=None):
         body = {'title': title, 'mimeType': mime_type}
         return self._upload(body, parent_id=parent_id)
