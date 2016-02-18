59564dcaab3071850e3d87590471807ef37c48e4
Andrean Franc <andrean.franc@gmail.com>
2016-02-18 19:27:54 +0100

    Handle empty user object creation

--- broadcast/util/auth.py
+++ broadcast/util/auth.py
@@ -84,11 +84,15 @@ class User(object):
         'options',
     )
 
-    def __init__(self, data, db=None):
+    def __init__(self, data=None, db=None):
         self._db = db or request.db.sessions
+        # if user logs out, an empty user object is needed
+        if not data:
+            data = dict((k, None) for k in self._columns)
+        # data must be json-serializable, make sure it is
         if not isinstance(data, dict):
-            # data must be json-serializable, make sure it is
             data = dict((k, data[k]) for k in data.keys())
+
         self._data = data
 
     def __getattr__(self, name):
