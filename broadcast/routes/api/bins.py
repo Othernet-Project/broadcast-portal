25c2658b55a62379f5156213ce5442e7a7a69078
Andrean Franc <andrean.franc@gmail.com>
2016-03-03 16:30:51 +0100

    Add endpoint to list items belonging to a specific bin

--- broadcast/routes/api/bins.py
+++ broadcast/routes/api/bins.py
@@ -1,5 +1,6 @@
 from .base import BaseAPI
 from ...util.bins import Bin
+from ...util.broadcast import filter_items, ContentItem
 
 
 class BinListAPI(BaseAPI):
@@ -10,8 +11,17 @@ class BinListAPI(BaseAPI):
         return {'results': results, 'count': len(results)}
 
 
+class BinItemsListAPI(BaseAPI):
+    __name__ = 'BinItemsListAPI'
+
+    def get(self, id):
+        results = filter_items(ContentItem.type, bin=id, raw=True)
+        return {'results': results, 'count': len(results)}
+
+
 def route(conf):
     return [
         ('/api/bin/', ['GET'], BinListAPI(), 'bin_list', {}),
+        ('/api/bin/<id>/items/', ['GET'], BinItemsListAPI(), 'bin_items_list', {}),
     ]
 
