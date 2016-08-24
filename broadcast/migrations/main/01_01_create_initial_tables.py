26088367ae7d4979359f7e6b8195824ac553cd61
Branko Vukelic <branko@outernet.is>
2016-08-25 01:01:13 +0200

    Set default value for numeric values
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- broadcast/migrations/main/01_01_create_initial_tables.py
+++ broadcast/migrations/main/01_01_create_initial_tables.py
@@ -4,31 +4,31 @@ create table bins
     id varchar primary_key unique,
     created timestamp,      -- creation timestamp
     closed timestamp,       -- finalization timestamp
-    size integer,           -- total size (bytes)
-    count integer           -- number of items in the bin
+    size integer default 0, -- total size (bytes)
+    count integer default 0 -- number of items in the bin
 );
 
 create table content
 (
     id varchar primary_key unique,
-    created timestamp,      -- creation timestamp
-    email varchar,          -- creator email
-    username varchar,       -- creator username
-    ipaddr varchar,         -- creator IP address
-    path varchar,           -- file path relative to upload root
-    size integer,           -- file size in bytes
-    bin varchar,            -- bin ID
-    votes integer,          -- number of votes
-    category varchar        -- category name
+    created timestamp,          -- creation timestamp
+    email varchar,              -- creator email
+    username varchar,           -- creator username
+    ipaddr varchar,             -- creator IP address
+    path varchar,               -- file path relative to upload root
+    size integer default 0,     -- file size in bytes
+    bin varchar,                -- bin ID
+    votes integer default 0,    -- number of votes
+    category varchar            -- category name
 );
 
 create table votes
 (
     id integer primary_key,
-    created timestamp,                  -- time when vote was cast
-    name varchar,                       -- voter's username
-    is_upvote varchar,                  -- whether vote is an upvote
-    content_id integer                  -- content
+    created timestamp,          -- time when vote was cast
+    name varchar,               -- voter's username
+    is_upvote varchar,          -- whether vote is an upvote
+    content_id integer          -- content
 );
 """
 
