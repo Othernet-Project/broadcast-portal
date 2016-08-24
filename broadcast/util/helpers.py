b20cec87734b9c0fc7058d84cce7a8c1d02c7fd3
Branko Vukelic <branko@outernet.is>
2016-08-25 01:38:13 +0200

    Fix hdatetime
    Make the name easier (???) to understand -> human_time
    
    Change the code so that the result is (ugly) translation-friendly, using
    ngettext instead of gettext for correct pluralization.
    
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- broadcast/util/helpers.py
+++ broadcast/util/helpers.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import os
 import time
 import decimal
@@ -5,7 +7,10 @@ import datetime
 
 import pytz
 from bottle import request
-from bottle_utils.i18n import dummy_gettext as _
+from bottle_utils.i18n import (
+    dummy_gettext as _,
+    dummy_ngettext as ngettext,
+)
 
 from ..app.exts import container as exts
 from .gdrive import DriveClient
@@ -54,23 +59,35 @@ def hamount(cent_amount, config=None):
 
 
 @template_helper
-def hdatetime(dt):
-    diff = datetime.datetime.utcnow() - dt
-    periods = (
-        (diff.days / 365, _("year"), _("years")),
-        (diff.days / 30, _("month"), _("months")),
-        (diff.days / 7, _("week"), _("weeks")),
-        (diff.days, _("day"), _("days")),
-        (diff.seconds / 3600, _("hour"), _("hours")),
-        (diff.seconds / 60, _("minute"), _("minutes")),
-        (diff.seconds, _("second"), _("seconds")),
-    )
-    for (period, singular, plural) in periods:
-        if period:
-            unit = singular if period == 1 else plural
-            return _("{period} {unit} ago").format(period=period, unit=unit)
+def human_time(dt):
+    diff = utcnow() - dt
 
-    return _("just now")
+    n = round(diff.days / 365)
+    if n >= 1:
+        return ngettext('{n} year ago', '{n} years ago', n).format(n=int(n))
+
+    n = round(diff.days / 365)
+    if n >= 1:
+        return ngettext('{n} month ago', '{n} months ago', n).format(n=int(n))
+
+    n = round(diff.days / 7)
+    if n >= 1:
+        return ngettext('{n} week ago', '{n} weeks ago', n).format(n=int(n))
+
+    n = diff.days
+    if n >= 1:
+        return ngettext('{n} day ago', '{n} days ago', n).format(n=int(n))
+
+    n = round(diff.seconds / 3600)
+    if n >= 1:
+        return ngettext('{n} hour ago', '{n} hours ago', n).format(n=int(n))
+
+    n = round(diff.seconds / 60)
+    if n >= 5:
+        return ngettext('{n} minute ago', '{n} minutes ago', n).format(
+            n=int(n))
+
+    return _('just now')
 
 
 @template_helper
