d128af370e0a9e363a500d2db1d17493b24ba4eb
Branko Vukelic <branko@outernet.is>
2016-08-29 22:23:20 +0200

    Check if user has already signed up
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- broadcast/forms/main.py
+++ broadcast/forms/main.py
@@ -1,10 +1,12 @@
+import time
+import random
 import logging
 
 from bottle_utils import form
 from bottle_utils.i18n import dummy_gettext as _
 
 from ..app.exts import container as exts
-from ..models.auth import InvitationToken
+from ..models.auth import User, InvitationToken
 from ..util.validators import EmailValidator
 
 
@@ -18,12 +20,23 @@ class BetaSignupForm(form.Form):
         validators=[form.Required(), EmailValidator()],
         placeholder=_('Email'))
 
+    def random_pause(self):
+        # Simulates sending emails
+        time.sleep(random.randint(2, 5))
+
     def validate(self):
         email = self.processed_data['email']
+        try:
+            User.get(email=email)
+        except User.NotFound:
+            pass
+        else:
+            # User already exits, so we bail here after a random timeout
+            self.random_pause()
+            return
         if email in exts.beta_whitelist:
             token = InvitationToken.new(email, BETA_SIGNUP_EXPIRY)
             token.send()
-            self.processed_data['token_sent'] = True
         else:
+            self.random_pause()
             logging.debug('BETA: %s', email)
-            self.processed_data['token_sent'] = False
