1f3da842d005ec86bee57f5d1baaf5f17ad348e7
Branko Vukelic <branko@outernet.is>
2016-09-05 12:04:55 +0200

    Use anonymous user when session is disabled
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- broadcast/plugins/auth.py
+++ broadcast/plugins/auth.py
@@ -12,14 +12,20 @@ def pre_init():
             logging.debug('Auth disabled')
             return
         if not hasattr(request, 'session'):
+            request.user = AnonymousUser()
+            logging.debug('Sessions, using anonymous user')
             return
         user_data = request.session.get('user')
         if user_data:
             request.user = User.from_json(user_data)
+            logging.debug('Created user from session data: %s',
+                          request.user.username)
         else:
             request.user = AnonymousUser()
+            logging.debug('Using anonymous user')
 
     @after
     def store_user(route):
         if hasattr(request, 'user') and not request.user.is_guest:
+            logging.debug('Storing user data in the session')
             request.session['user'] = request.user.to_json()
