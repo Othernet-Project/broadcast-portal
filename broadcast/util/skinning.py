48bbab997f51daa4dbb840b98bd424d1da517a99
Branko Vukelic <branko@outernet.is>
2016-09-05 16:35:45 +0200

    Fix skin setup not honoring external skins
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- broadcast/util/skinning.py
+++ broadcast/util/skinning.py
@@ -1,11 +1,17 @@
-from os.path import join, normpath
+from os.path import join, normpath, isdir
 
 from ..app.exts import container as exts
 
 
 def skin_dir():
-    return join(exts.root, normpath(exts.config['assets.skin_dir']),
-                exts.config['assets.skin'])
+    skindir = exts.config['assets.skin_dir']
+    extras = exts.config['assets.extra_skins']
+    skin = exts.config['assets.skin']
+    if extras:
+        skinpath = join(normpath(extras), skin)
+    if isdir(skinpath):
+        return skinpath
+    return join(exts.root, normpath(skindir), skin)
 
 
 def skin_view_dir():
